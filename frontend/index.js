9fd50d5eb633e270c6443b4408ec361f34436f00
eansengchang <76442284+eansengchang@users.noreply.github.com>
2022-01-01 12:21:29 +0000

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io.connect("http://127.0.0.1:3000");
+const socket = io.connect("https://pure-reaches-96587.herokuapp.com/");
 
 socket.on("init", handleInit);
 socket.on("gameState", handleGameState)
